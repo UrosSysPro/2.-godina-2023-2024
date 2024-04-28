c9776d48d2f58d11cfae73c7c64a5819da51e4bf
Uros <karaleicuu@gmail.com>
2024-04-28 15:48:45 +0200

    radi hexagon grid, pocetak drag and drop gird

--- components/glow-hexagon-grid/script.js
+++ components/glow-hexagon-grid/script.js
@@ -23,6 +23,17 @@ function drawHexagon(x,y,a,color){
 for(var i=0;i<100;i++){
 	for(var j=0;j<100;j++){
 		var a=30;
-		drawHexagon(i*2*a,j*2*a,a);
+		var x=a*Math.sqrt(3)*i;
+		if(j%2==0)x+=a*Math.sqrt(3)/2
+		var y=1.5*a*j;
+		var padding=3;
+		drawHexagon(x,y,a-padding);
 	}
+}
+
+function mousemove(e){
+	console.log("mousemove");
+	var div=document.getElementById("glow");
+	div.style.top=e.y+"px";
+	div.style.left=e.x+"px";
 }
